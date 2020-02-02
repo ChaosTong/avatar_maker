e663e1b528abc34f014088e4daf501beb15fd407
ChaosTong <easyulife@gmail.com>
2020-02-02 15:21:50 +0800

    update

--- js/do.js
+++ js/do.js
@@ -1,29 +1,29 @@
-let timer = null
-let startTime = ''
-let endTime = ''
-const label = document.getElementById('avatar_template')
+// let timer = null
+// let startTime = ''
+// let endTime = ''
+// const label = document.getElementById('avatar_template')
+let imgLoadPreview = document.getElementById("canvas-img")
+// label.addEventListener('touchstart', function () {
+//     startTime = +new Date()
+//     var canvas = document.getElementById('cvs');
+//     var image = canvas.toDataURL("image/png")
+//     label.setAttribute("crossOrigin",'Anonymous')
+//     label.src = image;
+//     timer = setTimeout(function () {
+//         // deleteBtn.style.display = 'block'
+//         // alert('xx');
+//     }, 700)
+// })
 
-label.addEventListener('touchstart', function () {
-    startTime = +new Date()
-    var canvas = document.getElementById('cvs');
-    var image = canvas.toDataURL("image/png")
-
-    label.src = image;
-    timer = setTimeout(function () {
-        // deleteBtn.style.display = 'block'
-        // alert('xx');
-    }, 700)
-})
-
-label.addEventListener('touchend', function () {
-  endTime = +new Date()
-  clearTimeout(timer)
-  if (endTime - startTime < 700) {
-    // 处理点击事件
-    // label.classList.add('selected')
-    // alert('00');
-  }
-})
+// label.addEventListener('touchend', function () {
+//   endTime = +new Date()
+//   clearTimeout(timer)
+//   if (endTime - startTime < 700) {
+//     // 处理点击事件
+//     // label.classList.add('selected')
+//     // alert('00');
+//   }
+// })
 
 document.getElementById('download').onclick = function(ev) {
     if (document.getElementById('avatar_img').src == '') 
@@ -61,16 +61,31 @@ function loadImage() {
     if(document.getElementById('upload').files.length == 0) return
     var imgUrl = window.URL.createObjectURL(document.getElementById('upload').files[0]);
     document.getElementById('avatar_img').src = imgUrl;
+    imgLoadPreview.style.display='none'
     drawToCanvas(imgUrl, document.getElementById('avatar_template').src);
 }
 
 function drawToCanvas(img1, img2){
+    var size=300
     var cvs = document.getElementById('cvs');
-    var size = 300;
-    cvs.width = size;
-    cvs.height = size;
-    var ctx = cvs.getContext('2d');
+    var ctx = cvs.getContext('2d')
+    var devicePixelRatio = window.devicePixelRatio || 1
+    var backingStoreRatio = ctx.webkitBackingStorePixelRatio ||
+                              ctx.mozBackingStorePixelRatio ||
+                              ctx.msBackingStorePixelRatio ||
+                              ctx.oBackingStorePixelRatio ||
+                              ctx.backingStorePixelRatio || 1
+    var ratio = devicePixelRatio / backingStoreRatio;		                    
+    ctx.mozImageSmoothingEnabled = false;
+    ctx.webkitImageSmoothingEnabled = false;
+    ctx.msImageSmoothingEnabled = false;
+    ctx.imageSmoothingEnabled = false;
+    cvs.width = size*ratio;
+    cvs.height = size*ratio;
+    ctx.scale(ratio,ratio)
+
     var image1 = new Image;
+    image1.setAttribute("crossOrigin",'Anonymous')
     image1.src = img1;
     image1.onload = function() {
         var width = image1.width < image1.height ? size : size * (image1.width / image1.height);
@@ -85,9 +100,14 @@ function drawToCanvas(img1, img2){
 
         ctx.drawImage(image1, -x, -y, width, height);
         var image2 = new Image;
+        image2.setAttribute("crossOrigin",'Anonymous')
         image2.src = img2;
         image2.onload = function() {
             ctx.drawImage(image2, 0, 0, size, size);
+            window.setTimeout(function(){
+                imgLoadPreview.src = cvs.toDataURL("image/png")
+                imgLoadPreview.style.display='block'
+            },2000)
         }
     
     }
